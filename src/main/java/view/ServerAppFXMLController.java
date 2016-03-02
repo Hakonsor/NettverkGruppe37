7eeea5ae74d0099ad11636301561f9805bfa28b3
Hakonsor <hakonsor@gmail.com>
2016-03-03 00:26:34 +0100

    prototype tab server

--- src/main/java/view/ServerAppFXMLController.java
+++ src/main/java/view/ServerAppFXMLController.java
@@ -1,7 +1,26 @@
 package view;
-
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 /**
  * Created by Simen on 28.02.2016.
  */
-public class ServerAppFXMLController {
+public class ServerAppFXMLController implements Initializable {
+
+    @FXML
+    private Button button_manual_off;
+ 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+
+    @FXML
+    private void onButtonClickAction(ActionEvent event) {
+    }
+    
 }
